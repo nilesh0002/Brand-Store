c3b2cb2332a048983ece34ae9b808d4e780b940e
nilesh0002 <nilesh.singh2024@lpu.in>
2026-07-07 19:53:18 +0530

    do

--- backend/src/utils/manage_admin.js
+++ backend/src/utils/manage_admin.js
@@ -16,11 +16,10 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: 'user' }
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
